88a1c07c93b044944c60cb160f2d87b4a64b6637
Wogayehu Gezahegn <46642267+w3gw@users.noreply.github.com>
2021-03-05 16:46:47 +0300

    Industry and Project Dropdowns added in settings

--- admin_site/forms.py
+++ admin_site/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 
-from admin_site.models import ChecklistMaster
+from admin_site.models import CompanyDropdownsMaster,ProjectDropDownsMaster
 
 
-class ChecklistMasterForm(forms.ModelForm):
+class CompanyDropdownsMasterForm(forms.ModelForm):
     class Meta:
-        model=ChecklistMaster
+        model=CompanyDropdownsMaster
         fields = ('name','chk_type',)
         widgets={
-            'name':forms.TextInput(attrs={'class':'form-control','placeholder':'CheckList Name'}),
+            'name':forms.TextInput(attrs={'class':'form-control','placeholder':'Company Lookup/Dropdown Name'}),
             'chk_type':forms.Select(attrs={'class':'form-control form-control-uniform'}),
+        }
+    
+class ProjectDropdownsMasterForm(forms.ModelForm):
+    class Meta:
+        model=ProjectDropDownsMaster
+        fields = ('name','dropdown_type',)
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control','placeholder':'Project Lookup/Dropdown Name'}),
+            'dropdown_type':forms.Select(attrs={'class':'form-control form-control-uniform'}),
         }
---
+++ admin_site/views/dropdowns.py
@@ -0,0 +1,86 @@
+
+from django.utils import timezone
+
+from django.shortcuts import render,redirect
+from django.views.generic import (View,CreateView,ListView,UpdateView)
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from admin_site.models import CompanyDropdownsMaster,ProjectDropDownsMaster
+from admin_site.forms import CompanyDropdownsMasterForm,ProjectDropdownsMasterForm
+
+class CreateCompanyDropdownsMaster(LoginRequiredMixin,CreateView):
+    model = CompanyDropdownsMaster
+    form_class = CompanyDropdownsMasterForm
+    template_name = ""
+
+    def form_valid(self,form):
+        chkmst = form.save(commit=False)
+        chkmst.created_by = self.request.user
+        chkmst.save()
+        messages.success(self.request,"Check List Added Successfully")
+        return redirect("admin:settings")
+
+class UpdateCompanyDropdownsMaster(LoginRequiredMixin,UpdateView):
+    model = CompanyDropdownsMaster
+    form_class = CompanyDropdownsMasterForm
+    template_name = "admin/accounts/check_list_update.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['flag'] = "company_dropdown"
+        return context
+
+    def form_valid(self,form):
+        chkmst = form.save(commit=False)
+        chkmst.lastupdated_by = self.request.user
+        chkmst.lastupdated_date = timezone.now()
+        chkmst.save()
+        messages.success(self.request,"Check List Updated Successfully")
+        return redirect("admin:settings") 
+
+class AllSettingsPage(LoginRequiredMixin,ListView):
+    model = CompanyDropdownsMaster
+    template_name = "admin/accounts/settings_list.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['chkform'] = CompanyDropdownsMasterForm()
+        context['projectform'] = ProjectDropdownsMasterForm()
+        context['pl_objects'] = ProjectDropDownsMaster.objects.all()
+        context['flag'] = "company_dropdown"
+        return context
+
+
+
+class CreateProjectDropdownsMaster(LoginRequiredMixin,CreateView):
+    model = ProjectDropDownsMaster
+    form_class = ProjectDropdownsMasterForm
+    template_name = ""
+
+    def form_valid(self,form):
+        chkmst = form.save(commit=False)
+        chkmst.created_by = self.request.user
+        chkmst.save()
+        messages.success(self.request,"Dropdown Added Successfully")
+        return redirect("admin:settings")
+
+class UpdateProjectDropdownsMaster(LoginRequiredMixin,UpdateView):
+    model = ProjectDropDownsMaster
+    form_class = ProjectDropdownsMasterForm
+    template_name = "admin/accounts/check_list_update.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['flag'] = "project_dropdown"
+        return context
+
+    def form_valid(self,form):
+        chkmst = form.save(commit=False)
+        chkmst.lastupdated_by = self.request.user
+        chkmst.lastupdated_date = timezone.now()
+        chkmst.save()
+        messages.success(self.request,"Dropdown Updated Successfully")
+        return redirect("admin:settings") 
+
+
