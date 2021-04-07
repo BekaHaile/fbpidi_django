4f6523af34230ec086a7b6c1f5a850f110edccb9
Antenyismu Yirsaw <55446297+Sevenstar-A@users.noreply.github.com>
2021-04-07 13:39:19 +0300

    Update api_views.py

--- collaborations/api/api_views.py
+++ collaborations/api/api_views.py
@@ -9,8 +9,8 @@ from rest_framework.response import Response
 from rest_framework import generics, permissions, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import permission_classes, authentication_classes,api_view
 from rest_framework import authentication, permissions
+from rest_framework.decorators import permission_classes, authentication_classes,api_view
 
 from company.models import Company, CompanyEvent, EventParticipants
 from collaborations.models import (PollsQuestion, PollsResult, Choices, News, NewsImages, Blog, BlogComment,
