1071fa7cdc4e8f1e81c26a76241d8cf8499a8720
Antenyismu Yirsaw <55446297+Sevenstar-A@users.noreply.github.com>
2021-04-16 16:56:53 +0300

    added the company search

--- company/api/api_urls.py
+++ company/api/api_urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from company.api.api_views import ApiCompanyByMainCategoryList, ApiCompanyDetailView, ApiProject, ApiProjectDetail
+from company.api.api_views import ApiCompanyByMainCategoryList, ApiSearchCompany, ApiCompanyDetailView, ApiProject, ApiProjectDetail
 
 
 urlpatterns = [
     #Company related urls    
     path("comp-by-main-category/",ApiCompanyByMainCategoryList.as_view(),name="api_company_category_main"),
+    path("search_company/", ApiSearchCompany.as_view(), name = "api_search_company"),
     # path("sup_comp-by-main-category/<option>/",SupCompanyByMainCategory.as_view(),name="suplier_category_main"),
     path("company-detail/", ApiCompanyDetailView.as_view(), name = "api_company_detail"),
 
