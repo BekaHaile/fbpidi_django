020c11d670a0b70a1b7f81bc86b28f379765e1a6
Antenyismu Yirsaw <55446297+Sevenstar-A@users.noreply.github.com>
2021-05-08 12:37:08 +0300

    some minor fixes

--- fbpims/settings.py
+++ fbpims/settings.py
@@ -135,9 +135,9 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'admindb',
+        'NAME': 'FBPIDI_IIMS',
         'USER': 'postgres',
-        'PASSWORD': 'cbe@ps4woga',
+        'PASSWORD': 'Fbpidi@123..',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
