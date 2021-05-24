8342e77d762bb611c462c5173f1f6a967c56fe9a
Antenyismu Yirsaw <55446297+Sevenstar-A@users.noreply.github.com>
2021-05-24 14:51:43 +0300

    Jj

--- product/api/api_views.py
+++ product/api/api_views.py
@@ -29,7 +29,7 @@ from product.api.serializer import (ProductFullSerializer, ProductInfoSerializer
 
 
 def filter_products_by_name(products_list, name):
-    return products_list.filter( Q(name= name) | Q(brand__brand_name__icontains = name) | Q(brand__product_type__sub_category_name__icontains=name)).distinct()
+    return products_list.filter( Q(name__icontains= name) | Q(brand__brand_name__icontains = name) | Q(brand__product_type__sub_category_name__icontains=name)).distinct()
 
 class ApiProductByCategory(APIView):
      def get(self, request):
